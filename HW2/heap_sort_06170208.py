3b01634918cdbed0f34711513152e9b252303022
sun-peihsuan <55570354+sun-peihsuan@users.noreply.github.com>
2019-11-07 18:34:40 +0800

    Update heap_sort_06170208.py

--- HW2/heap_sort_06170208.py
+++ HW2/heap_sort_06170208.py
@@ -1,24 +1,24 @@
 class Solution(object):
-  s=[]
-  def h(gg):
-      n=len(gg)
+    def __init__(self):
+        self.s = []
+    def heap_sort(self,nums):
+        global s
+        n=len(nums)
    
-      for i in range(n,-1,-1):
-          f=i
-          l=(i*2)+1
-          r=(i*2)+2
-          if l<n:
-              if gg[f]>gg[l]:
-                  gg[f],gg[l]=gg[l],gg[f]
-          if r<n:
-              if gg[f]>gg[r]:
-                  gg[f],gg[r]=gg[r],gg[f]
-
-      s.append(gg[0])
-      gg[0]=gg[-1]
-      gg.pop(-1)
-    
-      if len(gg)>0:
-          gg=h(gg)
+        for i in range(n,-1,-1):
+            f=i
+            l=(i*2)+1
+            r=(i*2)+2
+            if l<len(nums):
+                if nums[f]>nums[l]:
+                    nums[f],nums[l]=nums[l],nums[f]
+            if r<n:
+                if nums[f]>nums[r]:
+                    nums[f],nums[r]=nums[r],nums[f]
+        self.s.append(nums[0])
+        nums[0]=nums[-1]
+        nums.pop(-1)
+        if len(nums)>0:
+            nums=self.heap_sort(nums)
         
-      return s
+        return self.s
