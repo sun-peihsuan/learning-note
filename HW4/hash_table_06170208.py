20af3b821c2d51dfd1ed698d12b66dadebc066b4
sun-peihsuan <55570354+sun-peihsuan@users.noreply.github.com>
2019-12-06 23:20:58 +0800

    Update hash_table_06170208.py

--- HW4/hash_table_06170208.py
+++ HW4/hash_table_06170208.py
@@ -1,32 +1,70 @@
+from Crypto.Hash import MD5
+
 class ListNode:
     def __init__(self, val):
         self.val = val
         self.next = None
-        """
-        :type val: int
-        :type next: ListNode
-        :rtype: None        
-        """
+        
 class MyHashSet:
-    def __init__(self, capacity):
+    def __init__(self, capacity=5):
         self.capacity = capacity
         self.data = [None] * capacity
-        """
-        :type capacity: int
-        :rtype: None
-        """
+        
     def add(self, key):
-        """
-        :type key: str
-        :rtype: None
-        """
+        h=MD5.new()
+        h.update(key.encode("utf-8"))
+        h=h.hexdigest()
+        x=int(h,16)
+        y=x%self.capacity
+        node=self.data[y]
+        
+        a=ListNode(h)
+        a.next=self.data[y]
+        self.data[y]=a
+        
+     
     def remove(self, key):
-        """
-        :type key: str
-        :rtype: None
-        """
+        h=MD5.new()
+        h.update(key.encode("utf-8"))
+        h=h.hexdigest()
+        x=int(h,16)
+        y=x%self.capacity
+        node=self.data[y]
+        print(y)
+        
+        c=self.data[y]        
+        
+        if c.next == None:
+            if c.val==h:
+                self.data[y]=None
+          
+        while c.next:
+            print(5)
+            if c.next.val==h:
+                print(2)
+                c.next = c.next.next
+            if c.next.val!=h:
+                print(3)
+                c=c.next
+
+    
     def contains(self, key):
-        """
-        :type key: str
-        :rtype: bool(True or False)
-        """
+        h=MD5.new()
+        h.update(key.encode("utf-8"))
+        h=h.hexdigest()
+        x=int(h,16)
+        y=x%self.capacity
+        node=self.data[y]
+        
+        c=ListNode(h)
+        d=self.data[y]
+        
+        while d:
+            if c.val==d.val:
+                return True
+                break
+            else:
+                d=d.next
+        while d is None:
+            return False
+
