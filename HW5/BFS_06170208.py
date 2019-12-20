7eef3e76537c32262d8b5ddc6aa15d228a90833c
sun-peihsuan <55570354+sun-peihsuan@users.noreply.github.com>
2019-12-20 23:08:25 +0800

    Update BFS_06170208.py

--- HW5/BFS_06170208.py
+++ HW5/BFS_06170208.py
@@ -9,12 +9,45 @@ class Graph:
         self.graph[u].append(v) 
   
     def BFS(self, s): 
-        """
-        :type s: int
-        :rtype: list
-        """
+        n=len(self.graph)
+        check=[False]*n
+        Q=[]
+        p=[]
+        Q.append(s)
+        p.append(s)
+        check[s]=True
+        
+        a=False in check
+        while a==True and len(Q)!=0:
+            s=Q.pop(0)
+            for i in self.graph[s]:
+                if check[i] ==False:
+                    Q.append(i)
+                    p.append(i)
+                    check[i]=True
+            
+        return p
+    
+
+        
     def DFS(self, s):
-        """
-        :type s: int
-        :rtype: list
-        """
+        n1=len(self.graph)
+        check=[False]*n1
+        S=[]
+        p1=[]
+        S.append(s)
+        check[s]=True
+        
+        a=False in check
+        while a==True and len(S)!=0:
+            s=S.pop(-1)
+            for i in self.graph[s]:
+                if check[i] ==False:
+                    S.append(i)
+                    check[i]=True
+            p1.append(s)
+            
+        return p1
+            
+    
+        
